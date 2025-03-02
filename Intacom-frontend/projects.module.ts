383bf9b877ee77baff4afd332bd07002b95d5359
Manny Rivas <eamonrivas@gmail.com>
2025-03-02 11:20:43 -0800

    Changes 1 2/2

--- Intacom-frontend/projects.module.ts
+++ Intacom-frontend/projects.module.ts
@@ -1,13 +1,14 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
-import { ProjectsController } from "./projects.controller";
-import { Project } from './project.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Project, ProjectSchema } from '../../models/project.model';
 
 @Module({
-    import: [TypeOrmModule.forFeature([Project])],
-    providers: [ProjectsService],
-    controllers: [ProjectsController],
-    exports: [ProjectsService],
+  imports: [
+    MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+  ],
+  controllers: [ProjectsController],
+  providers: [ProjectsService],
 })
 export class ProjectsModule {}
