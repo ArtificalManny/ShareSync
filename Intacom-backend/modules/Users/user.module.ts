ad3aecef093370e7640337404a73cad72639d0ba
Manny Rivas <eamonrivas@gmail.com>
2025-03-11 20:54:26 -0700

    Adjust import paths in user.module.ts and auth.service.ts

--- Intacom-backend/modules/Users/user.module.ts
+++ Intacom-backend/modules/Users/user.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from '../src/models/user.model'; // Adjusted path
-import { AuthController } from '../src/modules/Users/auth/auth.controller'; // Adjusted path
-import { AuthService } from '../src/modules/Users/auth/auth.service'; // Adjusted path
+import { User, UserSchema } from '../../../src/models/user.model'; // Adjusted path: from ../../ to ../../../src
+import { AuthController } from '../../../src/modules/Users/auth/auth.controller'; // Adjusted path
+import { AuthService } from '../../../src/modules/Users/auth/auth.service'; // Adjusted path
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
--- Intacom-backend/src/modules/Users/auth/auth.service.ts
+++ Intacom-backend/src/modules/Users/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from '../../../models/user.model'; // Adjusted path: from ../../ to ../../../
+import { User } from '../../../models/user.model'; // Should be correct
 import * as bcrypt from 'bcrypt';
 import * as nodemailer from 'nodemailer';
 
