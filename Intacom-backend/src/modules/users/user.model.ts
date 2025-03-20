8dc3748d48fd8e03a45c9d06d5ea9cf788d8fa76
Manny Rivas <eamonrivas@gmail.com>
2025-03-19 22:01:31 -0700

    Add user.model.ts and uploads module files

---
+++ Intacom-backend/src/modules/users/user.model.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true, unique: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  birthday: { month: string; day: string; year: string };
+
+  @Prop()
+  profilePic: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
