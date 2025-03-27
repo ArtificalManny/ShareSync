b41e2247bd9fa1442b0089eed1f300ed35455ad1
Manny Rivas <eamonrivas@gmail.com>
2025-03-26 20:16:30 -0700

    Backend fixes

--- Intacom-backend/src/app.module.ts
+++ Intacom-backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from '@modules/users/users.module';
-import { ProjectsModule } from '@modules/projects/projects.module';
-import { AuthModule } from '@modules/users/auth.module';
-import { UploadsModule } from '@modules/uploads/uploads.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { AuthModule } from './modules/users/auth.module';
+import { UploadsModule } from './modules/uploads/uploads.module';
 
 @Module({
   imports: [
