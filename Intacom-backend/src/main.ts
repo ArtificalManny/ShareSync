98cc29706c0012dc9b7d8a4926845e78b91fad7b
Manny Rivas <eamonrivas@gmail.com>
2025-03-28 19:21:39 -0700

    Backend fixes

--- Intacom-backend/src/main.ts
+++ Intacom-backend/src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3001); // Changed from 3000 to 3001
-    console.log('Server running on port 3001');
+    await app.listen(3005); // Changed from 3004 to 3005
+    console.log('Server running on port 3005');
   } catch (error) {
     console.error('Error starting the server:', error);
     process.exit(1);
