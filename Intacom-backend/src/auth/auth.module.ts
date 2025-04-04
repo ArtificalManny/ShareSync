12078dcd2c35c17c20db267da65c6d93eecf19f7
Manny Rivas <eamonrivas@gmail.com>
2025-04-03 18:34:43 -0700

    Add NotificationsModule and PointsModule imports to AuthModule

--- Intacom-backend/src/auth/auth.module.ts
+++ Intacom-backend/src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from '../users/schemas/user.schema';
 import { UsersService } from '../users/users.service';
+import { NotificationsModule } from '../notifications/notifications.module'; // Import NotificationsModule
+import { PointsModule } from '../points/points.module'; // Import PointsModule
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    NotificationsModule, // Add NotificationsModule to provide NotificationsService
+    PointsModule, // Add PointsModule to provide PointsService
   ],
   controllers: [AuthController],
   providers: [AuthService, UsersService],
