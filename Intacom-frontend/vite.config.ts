52ecded2ffc7ddff1aab6b5024fae887e1ed83e8
Manny Rivas <eamonrivas@gmail.com>
2025-04-11 21:35:50 -0700

    Backend change

--- Intacom-frontend/vite.config.ts
+++ Intacom-frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
+        logLevel: 'debug', // Add debug logging to confirm proxy is working.
       },
     },
   },
