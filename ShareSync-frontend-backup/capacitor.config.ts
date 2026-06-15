0b04446b1b3100c54282c13819605943f549b87d
Manny Rivas <realmannyrivas@Mannys-MacBook-Pro.local>
2026-06-15 09:13:21 -0700

    Initialize Capacitor iOS app

---
+++ ShareSync-frontend-backup/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ca.openshare.app',
+  appName: 'OpenShare',
+  webDir: 'dist'
+};
+
+export default config;
